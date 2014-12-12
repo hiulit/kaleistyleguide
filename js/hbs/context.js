f1e368a65d587782f34642aca42e7c3c6d14d38c
hiulit <hiulit@gmail.com>
2014-12-12 16:26:34 +0100

    Fixed global variable

--- js/hbs/context.js
+++ js/hbs/context.js
@@ -1,36 +1,38 @@
-var mockupObjects = {
-	'filtertabs':  {
-		name: "Epeli",
-		tabs: [
-			{
-				selected: true,
-				label: 'Hola soy label',
-				id: '1'
-			},
-			{
-				selected: false,
-				label: 'Hola soy label 2',
-				id: '2'
-			}
-		],
-		author: {
-			firstName: "Alan",
-			lastName: "Johnson"
-		},
-		body: "I Love Handlebars",
-		comments: [
-			{
-				author: {
-					firstName: "Yehuda",
-					lastName: "Katz"
+define(function(){
+	return {
+		'filtertabs':  {
+			name: "Epeli",
+			tabs: [
+				{
+					selected: true,
+					label: 'Hola soy label',
+					id: '1'
 				},
-				body: "Me too!"
-			}
-		],
-		items: [
-			{name: "Handlebars", emotion: "love"},
-			{name: "Mustache", emotion: "enjoy"},
-			{name: "Ember", emotion: "want to learn"}
-		]
-	}
-};
+				{
+					selected: false,
+					label: 'Hola soy label 2',
+					id: '2'
+				}
+			],
+			author: {
+				firstName: "Alan",
+				lastName: "Johnson"
+			},
+			body: "I Love Handlebars",
+			comments: [
+				{
+					author: {
+						firstName: "Yehuda",
+						lastName: "Katz"
+					},
+					body: "Me too!"
+				}
+			],
+			items: [
+				{name: "Handlebars", emotion: "love"},
+				{name: "Mustache", emotion: "enjoy"},
+				{name: "Ember", emotion: "want to learn"}
+			]
+		}
+	};
+});
